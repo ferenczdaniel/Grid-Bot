78e5d55f7c785f98b361eb0ed49c3d8c2a2dcddd
ferencz.daniel <ferencz.daniel@codespring.ro>
2023-05-19 10:12:25 +0300

    string

--- gridbot-ui/src/App.jsx
+++ gridbot-ui/src/App.jsx
@@ -12,7 +12,7 @@ import {
 
 function App() {
     useState(() => {
-        window.reactACInterface?.resizeDialog(300, 50);
+        window.reactACInterface?.resizeDialog("300", "50");
     }, []);
 
     const [messages, setMessages] = useState([
@@ -27,7 +27,7 @@ function App() {
     const [isTyping, setIsTyping] = useState(false);
 
     const handleSend = async (message) => {
-        window.reactACInterface?.resizeDialog(300, 500);
+        window.reactACInterface?.resizeDialog("300", "500");
 
         const newMessage = {
             content: message,
